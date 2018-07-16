738e150a868ad10cad29b178781797f621568cfc
Ivan Babkin <ivan@babkin.ru>
2018-07-17 01:57:10 +0300

    добавлено меню с параметрами рисования

---
+++ SimpleDrawing/MenuView.swift
@@ -0,0 +1,71 @@
+//
+//  MenuView.swift
+//  SimpleDrawing
+//
+//  Created by Ivan Babkin on 16.07.2018.
+//  Copyright © 2018 Ivan Babkin. All rights reserved.
+//
+
+import UIKit
+
+protocol MenuViewDelegate: class {
+    func setParams(lineWidth: CGFloat, color: UIColor)
+}
+
+struct RGBColor {
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var alpha: CGFloat = 1.0
+    
+    var uiColor: UIColor {
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
+class MenuView: UIView {
+    var visible = false
+    var rgbColor = RGBColor()
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var lineWidthSlider: UISlider!
+    
+    weak var delegate: MenuViewDelegate!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let frame = UIScreen.main.bounds
+        self.frame = CGRect(x: -frame.width / 2.0, y: 0.0, width: frame.width / 2.0, height: frame.height)
+        
+        colorView.layer.borderColor = UIColor.black.cgColor
+        colorView.layer.borderWidth = 1.0
+        colorView.backgroundColor = rgbColor.uiColor
+
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1.0
+    }
+    
+    @IBAction func redChanging(_ sender: UISlider) {
+        rgbColor.red = CGFloat(sender.value)
+        colorView.backgroundColor = rgbColor.uiColor
+    }
+    
+    @IBAction func greenChanging(_ sender: UISlider) {
+        rgbColor.green = CGFloat(sender.value)
+        colorView.backgroundColor = rgbColor.uiColor
+    }
+    
+    @IBAction func blueChanging(_ sender: UISlider) {
+        rgbColor.blue = CGFloat(sender.value)
+        colorView.backgroundColor = rgbColor.uiColor
+    }
+    
+    @IBAction func alphaChanging(_ sender: UISlider) {
+        rgbColor.alpha = CGFloat(sender.value)
+        colorView.backgroundColor = rgbColor.uiColor
+    }
+    
+    func getParamsForDrawing() -> (CGFloat, UIColor) {
+        return (CGFloat(lineWidthSlider.value), rgbColor.uiColor)
+    }
+}
--- SimpleDrawing/ViewController.swift
+++ SimpleDrawing/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuView: MenuView!
+    @IBOutlet weak var menuButton: UIButton!
+    
+    var drawView: DrawView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let frame = menuButton.frame
+        menuButton.frame = CGRect(x: menuView.frame.maxX + 10, y: frame.minY, width: frame.width, height: frame.height)
+        
+        drawView = DrawView(frame: UIScreen.main.bounds)
+        self.view.addSubview(drawView)
+        self.view.sendSubview(toBack: drawView)
     }
-
-
+    
+    @IBAction func showMenu(_ sender: Any) {
+        UIView.animate(withDuration: 0.7, animations: {
+            if self.menuView.visible {
+                self.hideMenuAnumated()
+            }
+            else {
+                self.showMenuAnimated()
+            }
+        })
+    }
+    
+    func showMenuAnimated() {
+        self.menuView.frame = CGRect(x: 0, y: self.menuView.frame.minY, width: self.menuView.frame.width, height: self.menuView.frame.height)
+        self.menuButton.frame = CGRect(x: self.menuButton.frame.minX + self.menuView.frame.width, y: self.menuButton.frame.minY, width: self.menuButton.frame.width, height: self.menuButton.frame.height)
+        self.menuView.visible = true
+    }
+    
+    func hideMenuAnumated() {
+        self.menuView.frame = CGRect(x: -self.menuView.frame.width, y: self.menuView.frame.minY, width: self.menuView.frame.width, height: self.menuView.frame.height)
+        self.menuButton.frame = CGRect(x: self.menuButton.frame.minX - self.menuView.frame.width, y: self.menuButton.frame.minY, width: self.menuButton.frame.width, height: self.menuButton.frame.height)
+        self.menuView.visible = false
+        
+        let (lineWidth, color) = menuView.getParamsForDrawing()
+        drawView.color = color
+        drawView.lineWidth = lineWidth
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.menuView.visible {
+            hideMenuAnumated()
+        }
+        super.touchesBegan(touches, with: event)
+    }
+    
 }
 
