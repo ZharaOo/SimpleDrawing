8139a015111cdd3728c8343d0b129e61e2518017
Ivan Babkin <ivan@babkin.ru>
2018-07-16 20:28:19 +0300

    init commit

---
+++ SimpleDrawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleDrawing
+//
+//  Created by Ivan Babkin on 16.07.2018.
+//  Copyright © 2018 Ivan Babkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
